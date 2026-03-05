1742dbfa0d686921f21c499db45674620a8d1dce
mitarayushka <126961692+mitarayushka@users.noreply.github.com>
2026-03-05 19:08:22 +0300

    Update Task2.cs

--- Lab7/Green/Task2.cs
+++ Lab7/Green/Task2.cs
@@ -1,8 +1,122 @@
-﻿using System.Collections;
+using System.Collections;
 
 namespace Lab7.Green
 {
     public class Task2
     {
+        public struct Student
+        {
+            private string _name;
+            private string _surname;
+            private int[] _marks;
+            
+            public string Name => _name;
+            public string Surname => _surname;
+
+            public int[] Marks
+            {
+                get
+                {
+                    int[] copy = new int[_marks.Length];
+                    for (int i = 0; i < _marks.Length; i++)
+                    {
+                        copy[i] = _marks[i];
+                    }
+                    return copy;
+                }
+            }
+
+            public double AverageMark
+            {
+                get
+                {
+                    double sum = 0;
+                    int cnt = 0;
+                    for (int i = 0; i < _marks.Length; i++)
+                    {
+                        if (_marks[i] != 0)
+                        {
+                            sum += _marks[i];
+                            cnt++;
+                        }
+                    }
+
+                    if (cnt == 0)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        return sum / cnt;
+                    }
+                }
+            }
+
+            public bool IsExcellent
+            {
+                get
+                {
+                    for (int i = 0; i < _marks.Length; i++)
+                    {
+                        if (_marks[i] < 4)
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+
+            public Student(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _marks = new int[4];
+            }
+
+            public void Exam(int mark)
+            {
+                for (int i = 0; i < _marks.Length; i++)
+                {
+                    if (_marks[i] == 0)
+                    {
+                        _marks[i] = mark;
+                        break;
+                    }
+                }
+            }
+
+            public static void SortByAverageMark(Student[] array)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    for (int j = 0; j < array.Length - 1 - i; j++)
+                    {
+                        if (array[j].AverageMark < array[j + 1].AverageMark)
+                        {
+                            (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                        }
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"студент: {_name}, {_surname}");
+
+                for (int i = 0; i < _marks.Length; i++)
+                {
+                    if (_marks[i] == 0)
+                    {
+                        Console.WriteLine("_ ");
+                    }
+                    else
+                    {
+                        Console.Write(_marks[i]);
+                    }
+                }
+                
+            }
+        }
     }
 }
